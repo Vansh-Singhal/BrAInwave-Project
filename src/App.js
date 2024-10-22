554f00c93a4c06def2f530ef2fe5f9e72a8cebd3
Vansh Singhal <vansh.singhal@hotmail.com>
2024-10-22 21:40:03 +0530

    Redirectory enhanced

--- src/App.js
+++ src/App.js
@@ -1,21 +1,11 @@
 // src/App.js
 import React from 'react';
-import Header from './components/Header/Header';
-import HeroSection from './components/HeroSection/HeroSection';
-import FeaturesSection from './components/FeaturesSection/FeaturesSection';
-import ImpactSection from './components/ImpactSection/ImpactSection';
-import GetStartedSection from './components/GetStartedSection/GetStartedSection';
-import Footer from './components/Footer/Footer';
+import { LandingPage } from './pages/LandingPage';
 
 const App = () => {
   return (
     <div className="App">
-      <Header />
-      <HeroSection />
-      <FeaturesSection />
-      <ImpactSection />
-      <GetStartedSection />
-      <Footer />
+      <LandingPage/>
     </div>
   );
 };
