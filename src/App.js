bd577ac29fd6dd66dd9fb606b09350bb0908701e
Vansh Singhal <vansh.singhal@hotmail.com>
2024-10-24 18:42:57 +0530

    add routing

--- src/App.js
+++ src/App.js
@@ -5,14 +5,41 @@ import Header from './components/Header/Header';
 import Footer from './components/Footer/Footer';
 import CommunitiesPage from './pages/CommunitiesPage';
 import CommunityChat from './pages/CommunityChat';
+import { createBrowserRouter,RouterProvider } from 'react-router-dom';
+
 
 const App = () => {
+  const router = createBrowserRouter([
+    {path:"/",
+      element : <LandingPage/>
+    },
+    {path:"/community",
+      element : <CommunitiesPage/>
+    }
+    // ,
+    // {Path:"/blog",
+    //   element : <blog/>
+    // },
+    // {Path:"/about",
+    //   element : <about/>
+    // },
+    // {Path:"/features",
+    //   element : <features/>
+    // },
+    // {Path:"/contact",
+    //   element : <contact/>
+    // },
+    // {Path:"/signup",
+    //   element : <signup/>
+    // }
+  ])
   return (
     <div className="App">
       <Header/>
-      <LandingPage/>
-      {/* <CommunityChat/>
-      <CommunitiesPage/> */}
+      <RouterProvider router = {router}/>
+      {/* <LandingPage/> */}
+      {/* <CommunityChat/> */}
+      {/* <CommunitiesPage/>/ */}
       <Footer/>
     </div>
   );
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,18 +1,20 @@
 // src/components/Header/Header.js
 import React from 'react';
+import { LandingPage } from '../../pages/LandingPage';
+import CommunitiesPage from '../../pages/CommunitiesPage';
 
 const Header = () => {
+  
   return (
     <header className="bg-white shadow-sm">
       <nav className="container mx-auto px-4 py-4 flex justify-between items-center">
-        <a href="#" className="text-2xl font-bold text-indigo-500">ExamAssist</a>
+        <a href="/" className="text-2xl font-bold text-indigo-500">ExamAssist</a>
         <div className="space-x-4">
-          <a href="#" className="text-gray-600 hover:text-indigo-500">Community</a>
-          <a href="#" className="text-gray-600 hover:text-indigo-500">Blog</a>
-          <a href="#" className="text-gray-600 hover:text-indigo-500">About</a>
-          <a href="#" className="text-gray-600 hover:text-indigo-500">Features</a>
-          <a href="#" className="text-gray-600 hover:text-indigo-500">Contact</a>
-          <a href="#" className="bg-indigo-500 text-white px-4 py-2 rounded-md hover:bg-indigo-800">Sign Up</a>
+          <a href="/community" className="text-gray-600 hover:text-indigo-500">Community</a>
+          <a href="/blog" className="text-gray-600 hover:text-indigo-500">Blog</a>
+          <a href="/about" className="text-gray-600 hover:text-indigo-500">About</a>
+          <a href="/features" className="text-gray-600 hover:text-indigo-500">Features</a>
+          <a href="/signup" className="bg-indigo-500 text-white px-4 py-2 rounded-md hover:bg-indigo-800">Sign Up</a>
         </div>
       </nav>
     </header>
