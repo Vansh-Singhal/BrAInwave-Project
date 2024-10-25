4566569308584a7f8ecd22b05211ef7eb22bbf38
Vansh Singhal <vansh.singhal@hotmail.com>
2024-10-26 01:06:58 +0530

    logo added

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -10,9 +10,8 @@ const Header = () => {
   return (
     <header className="bg-white shadow-sm">
       <nav className="container mx-auto px-4 py-4 flex justify-between items-center">
-        <Link to="/" className="text-2xl font-bold text-indigo-500">
-        EYELINK
-          {/* <img src='D:\ReactJS\brainwave_project\src\resources\images\louda.jpg'/> */}
+        <Link to="/" className="text-2xl font-bold text-indigo-500 flex">
+          <div className="logodiv"></div> EXAM ASSIST
         </Link>
         <div className="space-x-4">
           <Link to="/community" className="text-gray-600 hover:text-indigo-500">Community</Link>
